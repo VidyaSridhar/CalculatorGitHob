0955e7d3c352c0a86feb3a52f187d97ffd8c6c33
Vidya <vsridh15@asu.edu>
2016-02-22 12:43:45 -0700

    Commit 3

--- vidya_test/src/cse360assign3/Calculator.java
+++ vidya_test/src/cse360assign3/Calculator.java
@@ -6,43 +6,96 @@ package cse360assign3;
  * PIN: 814
  * Title: Assignment 3
  * File name: Calculator.java
- * @version February 20, 2016
+ * @version February 22, 2016
  */
 public class Calculator {
 	
 private int total;
+public String log="";
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Description of getToTal - returns the total at each instant
+	 * @return totalmof type int
+	 */
 	public int getTotal () {
 			return total;
 	}
 	
+	/**
+	 * Description of add : Adds the value to the total
+	 * adds the history to the string log
+	 * @param value - value to be added
+	 */
 	public void add (int value) {
+		
 		total = total + value;
-		//return total;
+		if(log != "")
+			log = log + " " + "+" + " " + value;
+			
+		else
+			log = 0 + " " + "+" + " "  + value;
+						
 	}
 	
+	/**
+	 * Decsiption of subtract : Subtracts the value from the total
+	 * adds the history to the string log
+	 * @param value - value to be subtracted
+	 */
 	public void subtract (int value) {
+		
 		total = total - value;
-		//return total;
+		if(log != "")
+			log = log  + " " + "-" + " " + value;
+			
+		else
+			log = 0 + " " + "-" + " "  + value;
+			
 	}
 	
+	/**
+	 * Description of multiply: Multiplies the value with the total
+	 * adds the calculation history to the log
+	 * @param value - value to multiply
+	 */
 	public void multiply (int value) {
 		total = total * value;
+		if(log != "")
+			log = log + " " + "*" + " "+ value;
+		else
+			log = 0 + " " + "*" + " "+  value;
+			
 	}
 	
+	/**
+	 * Description of divide : Divides the total by the value
+	 * adds the calculation history to the string log
+	 * @param value - divisor-value to divide
+	 * if parameter value is 0, division operation should return 0
+	 */
 	public void divide (int value) {
 		if(value != 0)
 			total = total / value;
 		else
 			total = 0;
+		if(log != "")
+			log = log + " " +  "/" + " "+ value;
+		
+		else
+			log = 0  + " " + "/" + " "  + value;
 	}
 	
+	/**
+	 * Description of getHistory() : returns the string of the list of the operations performed
+	 * after every arithmetic operation, the history is appended to the string
+	 * @return string log
+	 */
 	public String getHistory () {
-		return "";
+		return log;
 	}
 
 }
--- vidya_test/src/cse360assign3/CalculatorTest.java
+++ vidya_test/src/cse360assign3/CalculatorTest.java
@@ -1,5 +1,6 @@
 package cse360assign3;
 // CalculatorGitHob/vidya_test/src/cse360assign3/CalculatorTest.java 
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -8,28 +9,25 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
+	//checks that the constructor doesn't return null
 	@Test
 	public void testCalculator() {
 		Calculator cal = new Calculator();
 		org.junit.Assert.assertNotNull(cal);
 	}
 
+	//tests if the total method returns the correct total
 	@Test
 	public void testGetTotal() {
 		//fail("Not yet implemented");
 		Calculator cal = new Calculator();
+		org.junit.Assert.assertEquals(0, cal.getTotal());
+		//tests for correct updation
 		cal.add(5);
 		org.junit.Assert.assertEquals(5, cal.getTotal());
 	}
 
+	//tests for Add function
 	@Test
 	public void testAdd() {
 		//fail("Not yet implemented");
@@ -40,33 +38,39 @@ public class CalculatorTest {
 		org.junit.Assert.assertEquals(15, cal.getTotal());
 	}
 
+	//tests for subtract function
 	@Test
 	public void testSubtract() {
 		//fail("Not yet implemented");
 		Calculator cal = new Calculator();
 		cal.subtract(1);
 		org.junit.Assert.assertEquals(-1, cal.getTotal());
+		//simultaneous operations
 		cal.add(3);
 		cal.subtract(1);
 		org.junit.Assert.assertEquals(1, cal.getTotal());	
 	}
 
-
+	//tests for multiplication
 	@Test
 	public void testMultiply() {
 		//fail("Not yet implemented");
 		Calculator cal = new Calculator();
+		//0*5 = 0
 		cal.multiply(5);
 		org.junit.Assert.assertEquals(0, cal.getTotal());
 		cal.add(1);
 		cal.multiply(5);
+		//1*5 = 5
 		org.junit.Assert.assertEquals(5, cal.getTotal());	
 	}
 
+	//tests for division
 	@Test
 	public void testDivide() {
 		//fail("Not yet implemented");
 		Calculator cal = new Calculator();
+		//tests division by 0 - if parameter is 0, should return 0
 		cal.divide(0);
 		org.junit.Assert.assertEquals(0, cal.getTotal());
 		cal.add(6);
@@ -74,9 +78,23 @@ public class CalculatorTest {
 		org.junit.Assert.assertEquals(3, cal.getTotal());	
 	}
 
+	//tests for gethistory
 	@Test
 	public void testGetHistory() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Calculator cal = new Calculator();
+		cal.add(2);
+		cal.subtract(3);
+		cal.multiply(5);
+		cal.add(6);
+		cal.divide(1);
+		cal.add(5);
+		cal.add(5);
+		cal.add(8);
+		cal.multiply(19);
+		cal.divide(5);
+		org.junit.Assert.assertEquals("0 + 2 - 3 * 5 + 6 / 1 + 5 + 5 + 8 * 19 / 5", cal.getHistory());
+		
 	}
 
 }
